8ba06d5a54aea83423564ecd7b54da89eee549b2
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-10-11 19:36:39 +0530

    Create:-Multiple Pages using react router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+    </Routes>
   )
 }
 
