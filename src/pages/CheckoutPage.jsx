4b31887c909d91cbee6c6e0e6900b4dc80c98e52
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-10-21 01:52:31 +0530

    Split:-Header into a component and use Link.

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -4,28 +4,28 @@ import './CheckoutPage.css';
 export function CheckoutPage(){
     return(
         <>
-            <title>Checkout</title>
-            <div className="checkout-header">
-      <div className="header-content">
-        <div className="checkout-header-left-section">
-          <a href="/">
-            <img className="logo" src="images/logo.png" />
-            <img className="mobile-logo" src="images/mobile-logo.png" />
-          </a>
-        </div>
+          <title>Checkout</title>
+          <div className="checkout-header">
+            <div className="header-content">
+              <div className="checkout-header-left-section">
+                <a href="/">
+                  <img className="logo" src="images/logo.png" />
+                  <img className="mobile-logo" src="images/mobile-logo.png" />
+                </a>
+              </div>
 
-        <div className="checkout-header-middle-section">
-          Checkout (<a className="return-to-home-link"
-            href="/">3 items</a>)
-        </div>
+              <div className="checkout-header-middle-section">
+                Checkout (<a className="return-to-home-link"
+                  href="/">3 items</a>)
+              </div>
 
-        <div className="checkout-header-right-section">
-          <img src="images/icons/checkout-lock-icon.png" />
-        </div>
-      </div>
+              <div className="checkout-header-right-section">
+                <img src="images/icons/checkout-lock-icon.png" />
+              </div>
             </div>
+          </div>
 
-            <div className="checkout-page">
+          <div className="checkout-page">
       <div className="page-title">Review your order</div>
 
       <div className="checkout-grid">
@@ -216,7 +216,7 @@ export function CheckoutPage(){
             </button>
         </div>
       </div>
-            </div>
+        </div>
         </>
     );
 }
