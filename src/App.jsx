30084aee17f836ba8956e506131a7af4cb35cdf1
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-10-22 20:19:03 +0530

    Create:-TackingPage.jsx and TrackingPage.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { CheckoutPage } from './pages/Checkout/CheckoutPage'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<HomePage />}></Route>
       <Route path='checkout' element={<CheckoutPage />} />
       <Route path='orders' element={<OrdersPage />} />
+      <Route path='tracking' element={<TrackingPage />} />
     </Routes>
   )
 }
