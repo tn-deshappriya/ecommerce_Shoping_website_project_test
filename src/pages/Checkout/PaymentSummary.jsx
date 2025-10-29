be3e77ec471c0395b295928b9ee6164d096eb622
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-10-29 15:17:27 +0530

    Create:- Order Button Work.

--- src/pages/Checkout/PaymentSummary.jsx
+++ src/pages/Checkout/PaymentSummary.jsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { formatMoney } from "../../utils/money";
 
-export function PaymentSummary({ paymentSummary }) {
+export function PaymentSummary({ paymentSummary, loadCart }) {
+
+    const navigate = useNavigate();
+
+    const createOrder = async () => {
+        await axios.post(`/api/orders`);
+        await loadCart();
+        navigate('/orders');
+    };
+
     return (
         <div className="payment-summary">
             <div className="payment-summary-title">
@@ -44,7 +55,8 @@ export function PaymentSummary({ paymentSummary }) {
                         </div>
                     </div>
 
-                    <button className="place-order-button button-primary">
+                    <button className="place-order-button button-primary"
+                        onClick={createOrder}>
                         Place your order
                     </button>
                 </>
